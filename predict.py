f1d3bf7e529a05e8abe482a3023fb103691a366e
Kamini <kaminikotekar@gmail.com>
2018-08-15 20:45:01 +0530

    modified

--- predict.py
+++ predict.py
@@ -7,3 +7,23 @@ Created on Wed Aug 15 15:12:55 2018
 
 from keras.models import load_model
 import cv2
+import numpy as np
+
+model= load_model('conv_model.h5')
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+img1=cv2.imread('testC1.jpg')
+img2=cv2.imread('testB.jpg')
+img1=cv2.resize(img1,(64,64))
+img2=cv2.resize(img2,(64,64))
+#img1=np.expand_dims(img1, axis=0)
+#img2=np.expand_dims(img2, axis=0)
+
+listl=[]
+listl.append(img1)
+listl.append(img2)
+a=np.array(listl)
+classes=model.predict_classes(a) 
+
+
