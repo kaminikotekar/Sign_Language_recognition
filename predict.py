4b3f6d5327ba8a1e4a9efcc1a456db05ed2ba2cb
Kamini <kaminikotekar@gmail.com>
2018-08-17 13:27:00 +0530

    modified

--- predict.py
+++ predict.py
@@ -8,22 +8,39 @@ Created on Wed Aug 15 15:12:55 2018
 from keras.models import load_model
 import cv2
 import numpy as np
+import os
 
 model= load_model('conv_model.h5')
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-img1=cv2.imread('testC1.jpg')
-img2=cv2.imread('testB.jpg')
-img1=cv2.resize(img1,(64,64))
-img2=cv2.resize(img2,(64,64))
+os.chdir('.\\screens')
+
+
+img_list=[]
+for files in os.listdir('.\\'):
+    i=1
+    print(files)
+    img=cv2.imread(files)
+    img=cv2.resize(img,(64,64))
+    img_list.append(img)
+    
+
+a=np.array(img_list)
+classes=model.predict_classes(a)
+
+
+img1=cv2.imread('1alpha.png')
+#img2=cv2.imread('testB.jpg')
+#img1=cv2.resize(img1,(64,64))
+#img2=cv2.resize(img2,(64,64))
 #img1=np.expand_dims(img1, axis=0)
 #img2=np.expand_dims(img2, axis=0)
 
-listl=[]
-listl.append(img1)
-listl.append(img2)
-a=np.array(listl)
-classes=model.predict_classes(a) 
+#listl=[]
+#listl.append(img1)
+#listl.append(img2)
+#a=np.array(listl)
+#classes=model.predict_classes(a) 
 
 
